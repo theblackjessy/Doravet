1ca8cad474d2d9030d3606fc950d3df295d50076
haybuka <paschal.nduleo@gmail.com>
2023-10-10 18:54:27 +0100

    overview stack

--- src/pages/dashboard/overview/index.js
+++ src/pages/dashboard/overview/index.js
@@ -23,22 +23,22 @@ const Dashboard = ({ children }) => {
   }
   return (
     <section className='w-full h-screen overflow-y-scroll'>
-      <aside className='flex my-12 gap-6 items-center' >
-        <div className='border-[#095494] border rounded-[10px] p-[20px] text-[#095494] w-[300px] bg-[#F0F8FF] flex gap-3'>
+      <aside className='md:flex my-12 gap-6 items-center' >
+        <div className='border-[#095494] border rounded-[10px] p-[20px] text-[#095494] my-6 md:my-0 md:w-[300px] bg-[#F0F8FF] flex gap-3'>
           <div className='py-5'> <PollQuestion /></div>
           <div className='flex flex-col '>
             <h3 className='font-bold text-[32px]' >3</h3>
             <h3 className='text-[16px]'>Poll questions</h3>
           </div>
         </div>
-        <div className='border-[#CE6300] border rounded-[10px] p-[20px] text-[#CE6300] w-[300px] bg-[#FFF8F2] flex gap-3 capitalize'>
+        <div className='border-[#CE6300] border rounded-[10px] p-[20px] text-[#CE6300] my-6 md:my-0 md:w-[300px] bg-[#FFF8F2] flex gap-3 capitalize'>
           <div className='py-5'> <PollOption /></div>
           <div className='flex flex-col '>
             <h3 className='font-bold text-[32px]' >12</h3>
             <h3 className='text-[16px]'>Poll options</h3>
           </div>
         </div>
-        <div className='border-[#026C1A] border rounded-[10px] p-[20px] text-[#026C1A] w-[300px] bg-[#F4FFF7] flex gap-3 capitalize'>
+        <div className='border-[#026C1A] border rounded-[10px] p-[20px] text-[#026C1A] my-6 md:my-0 md:w-[300px] bg-[#F4FFF7] flex gap-3 capitalize'>
           <div className='py-5'> <PollVoters /></div>
           <div className='flex flex-col '>
             <h3 className='font-bold text-[32px]' >12</h3>
